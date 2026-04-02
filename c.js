ecfd7d122cdb8c1d074513ae136533aad1fae74c
Ángel Javier Gil <angeljavgil@gmail.com>
2026-04-02 12:49:12 +0200

    Update c.js

--- c.js
+++ c.js
@@ -1,6 +1,6 @@
 (function () {
   const cg_tag = document.createElement("script");
-  cg_tag.src = "https://cdn.jsdelivr.net/gh/anggilb/scripts/t.js?v=1775124137547";
+  cg_tag.src = "https://cdn.jsdelivr.net/gh/anggilb/scripts@ad46d31/t.js?v=1775124137547";
   cg_tag.async = true;
   cg_tag.defer = true;
   document.getElementsByTagName("body")[0].appendChild(cg_tag);
