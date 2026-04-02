5051d660aab9eaf37456eae12672ef7c7a1635b0
Ángel Javier Gil <angeljavgil@gmail.com>
2026-04-02 12:34:20 +0200

    Update c.js

--- c.js
+++ c.js
@@ -1,6 +1,6 @@
 (function () {
   const cg_tag = document.createElement("script");
-  cg_tag.src = "https://cdn.cartsguru.io/e7bbbfe8-22f8-4bc2-a674-d739394ad3f5/t.js?v=1775124137547";
+  cg_tag.src = "https://cdn.jsdelivr.net/gh/anggilb/scripts@main/t.js?v=1775124137547";
   cg_tag.async = true;
   cg_tag.defer = true;
   document.getElementsByTagName("body")[0].appendChild(cg_tag);
