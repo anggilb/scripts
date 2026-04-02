972fc7a299e97f52f852fa8ec4660756a9b5e5aa
Ángel Javier Gil <angeljavgil@gmail.com>
2026-04-02 12:32:15 +0200

    Add files via upload

---
+++ t.js
@@ -0,0 +1,35 @@
+var trkParams = window.trkParams || {
+  trackerUrl: "https://cdn.cartsguru.io/scripts/20210823",
+  platform: "shopify-app",
+  siteId: "a5dff88d-aa84-42e3-8a7c-f4d63819ae51",
+  features: { ci: true, fbm: false, scoring: false, widgets: [] },
+  funnelMetaData: [],
+  funnelStepMetaData: [],
+  fbSettings: { app_id: "117034805405908", page_id: "" }
+};
+
+var cgtrkStart = function() {
+  CgTracker("init", trkParams);
+  CgTracker("track", { what: "event", ofType: "visit" });
+  window.onbeforeunload = function() {
+    console.timeStamp("window.onbeforeunload");
+    setTimeout(function() {
+      CgTracker("track", { what: "event", ofType: "quit" });
+    }, 0);
+  };
+};
+
+!(function(t, e, r) {
+  var a,
+    n = t.getElementsByTagName(e)[0];
+  if (!t.getElementById(r)) {
+    a = t.createElement(e);
+    a.id = r;
+    a.src =
+      "https://cdn.cartsguru.io/scripts/20210823/dist/shopify-client.min.js";
+    a.async = true;
+    a.defer = true;
+    n.parentNode.insertBefore(a, n);
+    a.onload = cgtrkStart;
+  }
+})(document, "script", "cg-trk");
